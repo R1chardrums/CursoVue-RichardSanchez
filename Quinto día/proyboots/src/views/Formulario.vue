0a0d90fc321ac0fbb63b2200787b2ec9b7132f77
r1chard <esteeserreh@gmail.com>
2020-09-16 19:05:09 -0400

    29/47 vídeos del curso Vue

---
+++ Quinto día/proyboots/src/views/Formulario.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="my-5">
+		<h1>Formulario</h1>
+		<form>
+			<b-row>
+				<b-col md="6">
+					<b-form-input type="text" v-model="texto" :state="comprobar"></b-form-input>
+					<small>Mínimo, 3 caracteres</small>
+					<b-form-input type="number"></b-form-input>
+					<p>{{texto}}</p>					
+				</b-col>	
+			</b-row>
+		</form>
+	</div>
+</template>
+
+<script>
+	export default{
+		name: 'Formulario',
+		data(){
+			return{
+				texto:''
+			}
+		},
+		computed:{
+			comprobar(){
+				return this.texto.length > 2 ? true : false
+			}
+		}
+	}
+</script>
---
+++ Quinto día/proyecto/src/views/Fotos.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="Fotos">
+    <h1>Página de fotos</h1>
+    <router-link :to="{name: 'Fotos', params:{id: item}}" 
+    v-for="(item, index) of lasFotos" :key="index">
+    	<button>Foto {{item}}</button>
+    </router-link>
+	<Foto/>
+	<button @click="home">Inicio</button>
+	<button @click="anterior">Anterior</button>
+	<button @click="siguiente">Siguiente</button>
+  </div>
+</template>
+
+<script>
+import Foto from "@/components/FotoComponent.vue";
+
+export default{
+	components:{
+		Foto
+	},
+
+	data(){
+		return{
+			lasFotos: [1,2,3]
+		}
+	},
+
+	methods:{
+		home(){
+			this.$router.push('/')
+		},
+		anterior(){
+			this.$router.go(-1)
+		},
+		siguiente(){
+			this.$router.go(1)
+		}
+	}
+}
+</script>
